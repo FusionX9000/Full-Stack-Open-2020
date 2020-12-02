8c56416d6b04fdb6d2a6d99281b467e365a27fbc
Salman Khan <2805979+FusionX9000@users.noreply.github.com>
2020-12-03 01:59:45 +0530

    completed part 3c

--- examples/part3/index.js
+++ examples/part3/index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const Note = require("./models/note");
 
 app.use(express.json());
 app.use(cors());
@@ -32,43 +34,56 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/notes", (request, response) => {
-  response.json(notes);
+  Note.find({}).then((result) => response.json(result));
 });
 
-app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  console.log(id);
-  const note = notes.find((note) => note.id === id);
-  if (note) response.json(note);
-  else response.status(404).end();
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/notes/:id", (request, response) => {
-  const id = request.params.id;
-  notes = notes.filter((note) => note.id !== id);
-  response.status(204).end();
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then((result) => response.status(204).end())
+    .catch((error) => next(error));
 });
 
-const generateId = () => {
-  const maxId =
-    notes.length > 0 ? Math.max(...notes.map((note) => note.id)) : 0;
-  return maxId + 1;
-};
+app.put("/api/notes", (request, response, next) => {
+  const body = response.body;
+  const note = new Note({
+    content: body.content,
+    important: body.important,
+  });
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => response.json(updatedNote))
+    .catch((error) => next(error));
+});
+
+// const generateId = () => {
+//   const maxId =
+//     notes.length > 0 ? Math.max(...notes.map((note) => note.id)) : 0;
+//   return maxId + 1;
+// };
 
 app.post("/api/notes", (request, response) => {
   const body = request.body;
   if (!body.content) {
     return response.status(400).json({ error: "content missing" });
   }
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
-  };
-  notes = notes.concat(note);
-  response.json(note);
+  });
+  note.save().then((savedNote) => response.json(note));
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => `server running on ${PORT}`);
