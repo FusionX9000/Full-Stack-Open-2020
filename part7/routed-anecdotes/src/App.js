d818994352c31fa2d2d22f26fe25d96d9fdd9ff7
Salman Khan <2805979+FusionX9000@users.noreply.github.com>
2021-01-29 08:47:54 +0530

    completed exercise 7.4-7.6

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -4,9 +4,10 @@ import {
   Route,
   Link,
   useRouteMatch,
-  Redirect,
   useHistory,
 } from "react-router-dom";
+
+import { useField } from "./hooks";
 
 const Menu = () => {
   const padding = {
@@ -92,51 +93,46 @@ const Footer = () => (
 );
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState("");
-  const [author, setAuthor] = useState("");
-  const [info, setInfo] = useState("");
+  const { reset: resetContent, ...content } = useField("text");
+  const { reset: resetAuthor, ...author } = useField("text");
+  const { reset: resetInfo, ...info } = useField("text");
   const history = useHistory();
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     });
     history.push("/");
-    props.showNotification(`a new anecdote ${content} created!`);
+    props.showNotification(`a new anecdote ${content.value} created!`);
+  };
+
+  const handleReset = (e) => {
+    resetContent();
+    resetAuthor();
+    resetInfo();
   };
 
   return (
     <div>
       <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} onReset={handleReset}>
         <div>
           content
-          <input
-            name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input name="content" {...content} />
         </div>
         <div>
           author
-          <input
-            name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input name="author" {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input name="info" {...info} />
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
+        <button type="reset">reset</button>
       </form>
     </div>
   );
